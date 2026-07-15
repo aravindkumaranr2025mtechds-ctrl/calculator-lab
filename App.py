d5e94a1b852ba56965a7ed799d6aee6672517e01
aravind <aravindkumaran.r.2025.mtechds@rajalakshmi.edu.in>
2026-07-15 10:47:56 +0530

    added multiply feature

--- App.py
+++ App.py
@@ -1,14 +1,16 @@
 from calc_func import add, subtract
+from calc_multiply import multiply
 
 
 def main():
     print("""Select the function from the given options:
     1. Add
     2. Subtract
+    3. Multiply
 
     """)
 
-    choice = input("Enter your choice (1/2): ")
+    choice = input("Enter your choice (1/2/3): ")
 
     if choice == "1":
         a = float(input("Enter the first number: "))
@@ -19,6 +21,11 @@ def main():
         a = float(input("Enter the first number: "))
         b = float(input("Enter the second number: "))
         print(f"The result is: {subtract(a, b)}")
+
+    elif choice == "3":
+        a = float(input("Enter the first number: "))
+        b = float(input("Enter the second number: "))
+        print(f"The result is: {multiply(a, b)}")
 
     else:
         print("Invalid choice.")
